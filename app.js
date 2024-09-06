c4a9ccb2469e497d89c2a065551139ee4f0913ec
LaurenTinoDev <pedrolaurentino3@gmail.com>
2024-09-06 10:15:33 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 function buscarInsumo(){
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados");
-    console.log(section); // Verifica se a seção foi encontrada
-  
+    let campoPesquisa = document.getElementById("busca").value;
     // Inicializa uma string vazia para armazenar os resultados
+    if(campoPesquisa == "" && campoPesquisa != String){
+      section.innerHTML = "Campo de busca vazio ou insumo não encontrado"
+      return 
+    };
+
+    campoPesquisa = campoPesquisa.toLowerCase()
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    
   
     // Itera sobre cada item de dado e constrói o HTML dos resultados
     for (let dado of dados) {
-      resultados += `
+      titulo = dado.titulo.toLowerCase();
+      descricao = dado.descricao.toLowerCase();
+      tags = dado.tags.toLowerCase();
+      dado.titulo.includes(campoPesquisa);
+      if (titulo.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+        resultados += `
         <div class="item-resultado">
           <h2>
             <a href="#" target="_blank">${dado.titulo}</a>
@@ -17,6 +32,11 @@ function buscarInsumo(){
           <a href=${dado.link} target="_blank">Mais informações</a>
         </div>
       `;
+      }
+      
+    }
+    if(!resultados){
+      resultados = "<p>Nada foi encontrado</p>"
     }
   
     // Atribui o HTML gerado à seção de resultados
